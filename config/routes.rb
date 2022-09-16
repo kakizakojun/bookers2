c9cd4d84536bda181366c1ece2b057967d18cd4a
JunKakizako <111491185+kakizakojun@users.noreply.github.com>
2022-09-16 15:34:31 +0000

    [add]Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+
+  # get 'homes/top'
   devise_for :users
   root to:"homes#top"
+  resources :books
+  # get 'books/new'
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
