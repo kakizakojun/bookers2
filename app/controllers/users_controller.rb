3058c437af598d5d493c1237ff14dbda7920da21
JunKakizako <111491185+kakizakojun@users.noreply.github.com>
2022-09-17 13:22:28 +0000

    [add]途中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
+    @user = Books.all
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-
-  # get 'homes/top'
+# get 'homes/top'
   devise_for :users
-  root to:"homes#top"
+  root to: 'homes#top'
   resources :books
   # get 'books/new'
   # get 'books/index'
   # get 'books/show'
   # get 'books/edit'
+  get 'homes/about' => 'homes/about',as: 'about'
+  resources :users
+  # get 'users/show'
+  # get 'users/edit'
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
