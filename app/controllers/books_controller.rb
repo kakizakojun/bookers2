770afa7c3c2d150baa80b801ec7e01cd183dd243
JunKakizako <111491185+kakizakojun@users.noreply.github.com>
2022-09-19 11:33:03 +0000

    [add]部分テンプレート

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,26 @@
 class BooksController < ApplicationController
   def new
     @book = Book.new
+    @user = User.find(params[:id])
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to'/books/id'
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
 
   def show
+    @user = current_user
+    @book = Book.new
+    @books = Book.all
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,35 @@
 class UsersController < ApplicationController
 
   def index
-    @user = User.all
+    @user = current_user
+    @book = Book.new
+    @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
     @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    # @book = Book.new(book_params)
+    # book.save
+    # redirect_to '/books/id'
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   def edit
     @user = User.find(params[:id])
   end
+
+
+
 
   def user_params
     params.require(:user).permit(:name, :introduction, :image)
